e622ec4f5ea6bb3fc6fbd3b427dbe4d9ad1bcb6c
Osaulenko Oleksandr <oom2212@gmail.com>
2019-12-01 14:18:56 +0200

    hw1

---
+++ GoIt-JS/Module-01/js/task-5.js
@@ -0,0 +1,64 @@
+"use strict";
+
+let nameCountry;
+let credits = [100, 250, 170, 80, 120];
+
+nameCountry = prompt("В яку країну бажаєте доставку?");
+if (nameCountry === null) console.log("Canceled users");
+else {
+  nameCountry = nameCountry.toLowerCase();
+  switch (nameCountry) {
+    case "китай":
+      console.log(
+        "Доставка в",
+        nameCountry,
+        "буде коштувати",
+        credits[0],
+        "кредитів"
+      );
+      break;
+
+    case "чілі":
+      console.log(
+        "Доставка в",
+        nameCountry,
+        "буде коштувати",
+        credits[1],
+        "кредитів"
+      );
+      break;
+
+    case "австралія":
+      console.log(
+        "Доставка в",
+        nameCountry,
+        "буде коштувати",
+        credits[2],
+        "кредитів"
+      );
+      break;
+
+    case "індія":
+      console.log(
+        "Доставка в",
+        nameCountry,
+        "буде коштувати",
+        credits[3],
+        "кредитів"
+      );
+      break;
+
+    case "ямайка":
+      console.log(
+        "Доставка в",
+        nameCountry,
+        "буде коштувати",
+        credits[4],
+        "кредитів"
+      );
+      break;
+
+    default:
+      console.log("В вашей стране доставка не доступна");
+  }
+}
---
+++ GoIt-JS/Module-01/js/task-6.js
@@ -0,0 +1,20 @@
+"use strict";
+let input;
+let total = 0;
+let i=0;
+for (i; i <999; i++)
+{
+    input = prompt('введіть число');
+    console.log(typeof(input));
+    console.log(input);
+    if (!input && input!="") break;
+if (isNaN(input))  alert("Введіть коректні дані!!!");
+else
+{
+    input = Number(input);
+    total = total + input;
+}
+console.log(i);
+}
+if (i===0) console.log('Жодного значення не було введено');
+else console.log(' Загальна сума дорівнює:',total);
