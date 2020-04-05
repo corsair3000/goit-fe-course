55db51a1eb89dc902ad3fb67321351505f97e367
Osaulenko Oleksandr <oom2212@gmail.com>
2020-04-05 12:00:27 +0300

    add hw10

---
+++ GoIt-JS/module-10/src/index.js
@@ -0,0 +1,41 @@
+import menuList from './menu.json';
+import './style.css';
+import menuItemTemplate from './templates/menu-template.hbs';
+//-----------//
+const btnSwitchTheme = document.querySelector('.js-switch-input');
+const body = document.querySelector('.js-body');
+const section = document.querySelector('.js-menu');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+crateMenu(menuList);
+
+const curentTheme = localStorage.getItem('theme');
+curentTheme === Theme.LIGHT ? lightTheme() : darkTheme();
+
+btnSwitchTheme.addEventListener('change', setTheme);
+
+function crateMenu(menuList) {
+  const menuItem = menuList
+    .map(itemMenu => menuItemTemplate(itemMenu))
+    .join('');
+  section.insertAdjacentHTML('beforeend', menuItem);
+}
+
+function setTheme() {
+  btnSwitchTheme.checked ? darkTheme() : lightTheme();
+}
+
+function darkTheme() {
+  btnSwitchTheme.checked = true;
+  localStorage.setItem('theme', Theme.DARK);
+  body.classList.remove(Theme.LIGHT);
+  body.classList.add(Theme.DARK);
+}
+function lightTheme() {
+  localStorage.setItem('theme', Theme.LIGHT);
+  body.classList.remove(Theme.DARK);
+  body.classList.add(Theme.LIGHT);
+}
